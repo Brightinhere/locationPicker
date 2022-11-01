2a942d0b743fe2f81ba19345740399b5a37e6532
Automated Heroku Deploy <irfan.bilir@hva.nl>
2022-11-01 22:36:14 +0100

    Update map on click

--- screens/MedicScreen/MedicScreen.js
+++ screens/MedicScreen/MedicScreen.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react'
-import {Dimensions, StyleSheet, Text, View} from 'react-native'
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native'
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
+import {LocationAccuracy} from "expo-location/src/Location.types";
+import axios from "axios";
 
 export default function MedicScreen(props) {
-    // const backendUrl = "http://localhost:8081"
+    const backendUrl = "http://localhost:8081"
 
-    const backendUrl = "https://locationpicker.herokuapp.com"
+    // const backendUrl = "https://locationpicker.herokuapp.com"
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
 
@@ -14,12 +16,20 @@ export default function MedicScreen(props) {
         (async () => {
 
             let { status } = await Location.requestForegroundPermissionsAsync();
+            // let { prov } = await Location.enableNetworkProviderAsync();
+
+            // console.log(prov)
+            // if (prov != null) {
+            //     setErrorMsg('Permission to access higher location accuracy was denied');
+            //     return;
+            // }
+
             if (status !== 'granted') {
                 setErrorMsg('Permission to access location was denied');
                 return;
             }
 
-            let location = await Location.getCurrentPositionAsync({accuracy: 6, mayShowUserSettingsDialog: true});
+            let location = await Location.getCurrentPositionAsync({accuracy: LocationAccuracy.BestForNavigation, mayShowUserSettingsDialog: true});
             setLocation(location);
         })();
     }, []);
@@ -31,8 +41,31 @@ export default function MedicScreen(props) {
         text = JSON.stringify(location);
     }
 
+    async function updateLocation() {
+
+        axios.get(backendUrl + '/getLocation')
+            .then(response => {
+                // handle success
+                const locArray = response.data.location.location.split(' ')
+                console.log(locArray[0])
+                console.log(locArray[1])
+                setLocation({
+                    coords: {
+                        latitude: locArray[0],
+                        longitude: locArray[1]
+                    }
+                })
+            })
+            .catch(error => {
+                // handle error
+                console.log(error)
+            })
+    }
+
+
     return (
         <View>
+            <Button onPress={updateLocation} title={"Get Last Loc"}/>
             <Text>Medic Screen</Text>
             {location != null && location.coords != null &&
                 <View style={styles.container}>
